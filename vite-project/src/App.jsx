0a0ef6a3856551da9991d7daf1f0e636c0944fb6
gracy94 <gracielafaria94@gmail.com>
2023-02-23 11:17:26 -0300

    Components added to App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,15 @@
+import Form from "./components/Form"
+import Header from "./components/Header"
+import PetsList from "./components/PetsList"
 
 function App() {
 
   return (
-    <div className="App">
-      <h1>Hola Mundo</h1>
-    </div>
+    <>
+      <Header/>
+      <Form></Form>
+      <PetsList></PetsList>
+    </>
   )
 }
 

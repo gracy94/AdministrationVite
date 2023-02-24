12636ce774b27d052698d11226be14b997f68156
gracy94 <gracielafaria94@gmail.com>
2023-02-24 12:02:28 -0300

    Patient component added to PetsList

--- vite-project/src/components/PetsList.jsx
+++ vite-project/src/components/PetsList.jsx
@@ -1,8 +1,19 @@
+import Patient from "./Patient"
+
 const PetsList = () => {
   return (
-    <>
-        <h2>Pets List</h2>
-    </>
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+        <h2 className="font-black text-3xl text-center ">Pet Listing</h2>
+
+        <p className="text-lg mt-5 text-center mb-8">
+        Manage your {""}
+        <span className="text-red-400 font-bold">Patients and dates</span>
+        </p>
+
+        <Patient></Patient>
+        <Patient></Patient>
+        <Patient></Patient>
+    </div>
   )
 }
 

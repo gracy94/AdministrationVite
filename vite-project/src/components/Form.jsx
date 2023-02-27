503dceb9fff711fecc8bf8bdd179d9a959645ffe
gracy94 <gracielafaria94@gmail.com>
2023-02-27 10:55:52 -0300

    state variables and form validation added

--- vite-project/src/components/Form.jsx
+++ vite-project/src/components/Form.jsx
@@ -1,13 +1,40 @@
+import { useState, useEffect } from "react";
+
+
 const Form = () => {
+  const [name, setName] = useState('');
+  const [owner, setOwner] = useState('');
+  const [email, setEmail] = useState('');
+  const [discharge, setDischarge] = useState('');
+  const [symptoms, setSymptoms] = useState('');
+
+  const [error, setError] = useState(false)
+
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+    // Form validation
+    if([name, owner, email, discharge, symptoms].includes('')) {
+      setError(true)
+    } else {
+      setError(false)
+    }
+  }
+  
+  
   return (
-    <div className="md:w-1/2 lg:w-2/5">
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
       <h2 className="font-black text-3xl text-center ">Patient monitoring</h2>
       <p className="text-lg mt-5 text-center mb-8">
         Add patients {""}
         <span className="text-red-400 font-bold">Manage</span>
       </p>
 
-      <form className="bg-white shadow-md rounded-lg py-10 mb-10 px-5 border border-red-400">
+      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded-lg py-10 mb-10 px-5 border border-red-400">
+        {error && 
+            <div className="bg-red-700 text-white text-center p-3 uppercase font-bold mb-3 rounded-md">
+              <p>All fields are required</p>
+            </div>
+        }
         <div className="mb-5">
           <label htmlFor="pet" className="block text-gray-700 uppercase font-bold ">
             Pet's Name</label>
@@ -16,6 +43,8 @@ const Form = () => {
               type="text"
               placeholder="Pet's Name"
               className="w-full p-2 mt-2 placeholder-gray-400 border border-red-400 rounded-md"
+              value={name}
+              onChange = {(e)=> setName(e.target.value)}
           />
         </div>
         <div className="mb-5">
@@ -26,6 +55,8 @@ const Form = () => {
               type="text"
               placeholder="Owner's Name"
               className="w-full p-2 mt-2 placeholder-gray-400 border border-red-400 rounded-md"
+              value={owner}
+              onChange = {(e)=> setOwner(e.target.value)}
           />
         </div>
         <div className="mb-5">
@@ -36,6 +67,8 @@ const Form = () => {
               type="email"
               placeholder="Email"
               className="w-full p-2 mt-2 placeholder-gray-400 border border-red-400 rounded-md"
+              value={email}
+              onChange = {(e)=> setEmail(e.target.value)}
           />
         </div>
         <div className="mb-5">
@@ -45,6 +78,8 @@ const Form = () => {
               id="discharge"
               type="date"
               className="w-full p-2 mt-2 placeholder-gray-400 border border-red-400 rounded-md"
+              value={discharge}
+              onChange = {(e)=> setDischarge(e.target.value)}
           />
         </div>
         <div className="mb-5">
@@ -54,6 +89,8 @@ const Form = () => {
               id="symptoms"
               className="w-full p-2 mt-2 placeholder-gray-400 border border-red-400 rounded-md"
               placeholder="Describe the symptoms"
+              value={symptoms}
+              onChange = {(e)=> setSymptoms(e.target.value)}
           />
         </div>
         <input 

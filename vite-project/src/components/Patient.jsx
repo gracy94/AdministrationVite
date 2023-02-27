c990abd4af25285ea20432e66bd820377cad6a8b
gracy94 <gracielafaria94@gmail.com>
2023-02-27 12:14:47 -0300

    dinamic information for petsList for each patient created

--- vite-project/src/components/Patient.jsx
+++ vite-project/src/components/Patient.jsx
@@ -1,33 +1,32 @@
-const Patient = () => {
+const Patient = ({patient}) => {
+
+    const {name, owner, email, discharge, symptoms} = patient
   return (
-    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-lg border border-red-400">
+    <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-lg border border-red-400">
       <p className="font-bold mb-3 text-gray-700 uppercase">
         Name: {""}
-        <span className="font-normal  normal-case">Tobby</span>
+        <span className="font-normal  normal-case">{name}</span>
       </p>
 
       <p className="font-bold mb-3 text-gray-700 uppercase">
         Owner: {""}
-        <span className="font-normal  normal-case">Nestor</span>
+        <span className="font-normal  normal-case">{owner}</span>
       </p>
 
       <p className="font-bold mb-3 text-gray-700 uppercase">
         Email: {""}
-        <span className="font-normal  normal-case">email@email.com</span>
+        <span className="font-normal  normal-case">{email}</span>
       </p>
 
       <p className="font-bold mb-3 text-gray-700 uppercase">
         Discharge: {""}
-        <span className="font-normal  normal-case">March 25, 2023</span>
+        <span className="font-normal  normal-case">{discharge}</span>
       </p>
 
       <p className="font-bold mb-3 text-gray-700 uppercase">
         Symptoms: {""}
         <span className="font-normal  normal-case">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia,
-          aspernatur quod eum excepturi voluptates at similique vero, officiis,
-          nam exercitationem architecto alias numquam molestiae tempora nihil?
-          Voluptatibus expedita earum doloribus.
+        {symptoms}
         </span>
       </p>
     </div>
--- vite-project/src/components/PetsList.jsx
+++ vite-project/src/components/PetsList.jsx
@@ -1,6 +1,9 @@
 import Patient from "./Patient"
 
-const PetsList = () => {
+const PetsList = ({patients}) => {
+
+
+
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
         <h2 className="font-black text-3xl text-center ">Pet Listing</h2>
@@ -10,9 +13,14 @@ const PetsList = () => {
         <span className="text-red-400 font-bold">Patients and dates</span>
         </p>
 
-        <Patient></Patient>
-        <Patient></Patient>
-        <Patient></Patient>
+        {patients.map ( patient=> (
+          <Patient 
+            key={patient.id}
+            patient={patient}>
+          </Patient>
+        ))}
+
+        
     </div>
   )
 }

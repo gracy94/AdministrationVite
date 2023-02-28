1382ed38377b4cf04f1db27fdafa26733925a4be
gracy94 <gracielafaria94@gmail.com>
2023-02-28 11:48:31 -0300

    if else there are patients added

--- vite-project/src/components/PetsList.jsx
+++ vite-project/src/components/PetsList.jsx
@@ -1,28 +1,38 @@
 import Patient from "./Patient"
 
-const PetsList = ({patients}) => {
-
-
+const PetsList = ({ patients, setPatient}) => {
 
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-        <h2 className="font-black text-3xl text-center ">Pet Listing</h2>
+      {patients && patients.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center ">Pet Listing</h2>
 
-        <p className="text-lg mt-5 text-center mb-8">
-        Manage your {""}
-        <span className="text-red-400 font-bold">Patients and dates</span>
-        </p>
+          <p className="text-lg mt-5 text-center mb-8">
+            Manage your {""}
+            <span className="text-red-400 font-bold">Patients and Dates</span>
+          </p>
 
-        {patients.map ( patient=> (
-          <Patient 
-            key={patient.id}
-            patient={patient}>
-          </Patient>
-        ))}
+          {patients.map((patient) => (
+            <Patient 
+                key={patient.id} 
+                patient={patient}
+                setPatient = {setPatient}
+                ></Patient>
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center ">No patients</h2>
 
-        
+          <p className="text-lg mt-5 text-center mb-8">
+            Start adding {""}
+            <span className="text-red-400 font-bold">patients</span>
+          </p>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default PetsList

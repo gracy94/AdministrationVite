95b44687e31b204f6b4a5a146f642c47f1c2c912
gracy94 <gracielafaria94@gmail.com>
2023-02-28 11:50:39 -0300

    Header eddited

--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -1,7 +1,12 @@
 function Header() {
     
     return (
-        <h1>Header</h1>
+        <>
+            <h1 className="font-black text-4xl text-center md:w-2/3 mx-auto">
+                Pets monitoring {""}
+                <span className="text-red-400 ">Vet</span>
+            </h1>
+        </>
     )
 }
 
